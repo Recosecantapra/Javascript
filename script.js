50705915f814ae290bf9f3f746f2d1d0881971e2
John Smith <rexcapra@proton.me>
2025-08-24 16:39:47 +0200

    javascript test

---
+++ script.js
@@ -0,0 +1,10 @@
+let Score = 0;
+const ScoreCounter = document.getElementById("Score")
+const Button = document.getElementById("increaser")
+
+function increaseScore() {
+    Score++;
+    ScoreCounter.textContent = Score;
+}
+
+Button.addEventListener("click", increaseScore)
