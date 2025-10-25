9533efe882d2613645d6106cc0065bca32b21593
John Smith <rexcapra@proton.me>
2025-10-25 17:17:29 +0200

    second button fix

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const ScoreCounter = document.getElementById("Score")
 const Button = document.getElementById("increaser")
 const Button5 = document.getElementById("increaser5")
 
-function increaseScore() {
-    Score++;
+function increaseScore(increase) {
+    Score += increase;
     ScoreCounter.textContent = Score;
 }
 
-Button.addEventListener("click", increaseScore)
-Button.addEventListener("click", increaseScore*5)
+
+Button.addEventListener("click", () => increaseScore(1))
+Button5.addEventListener("click", () => increaseScore(5))
