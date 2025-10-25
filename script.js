703ce4f698a090b0c2686cb53a6a6f75af686937
John Smith <rexcapra@proton.me>
2025-10-25 14:50:48 +0200

    new button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let Score = 0;
 const ScoreCounter = document.getElementById("Score")
 const Button = document.getElementById("increaser")
+const Button5 = document.getElementById("increaser5")
 
 function increaseScore() {
     Score++;
@@ -8,4 +9,4 @@ function increaseScore() {
 }
 
 Button.addEventListener("click", increaseScore)
-
+Button.addEventListener("click", increaseScore*5)
