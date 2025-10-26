b176f64801ca675a7547b619263dc0c14c4ddc4f
John Smith <rexcapra@proton.me>
2025-10-26 18:05:58 +0100

    unlocking mechanism

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ let Score = 0;
 const ScoreCounter = document.getElementById("Score")
 const Button = document.getElementById("increaser")
 const Button5 = document.getElementById("increaser5")
+const Buy5 = document.getElementById("buy5")
+
+Button5.disabled = true;
 
 function increaseScore(increase) {
     Score += increase;
     ScoreCounter.textContent = Score;
 }
 
+function buy() {
+    if (Score >= 50) {
+        Score -= 50;
+        Button5.disabled = false;
+        ScoreCounter.textContent = Score;
+    }
+}
+
+
 
 Button.addEventListener("click", () => increaseScore(1))
+Buy5.addEventListener("click", () => buy())
 Button5.addEventListener("click", () => increaseScore(5))
